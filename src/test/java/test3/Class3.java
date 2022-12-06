1064d3c83c051894d53ab5ee676dd5f07c441f18
farhankhalid08 <farhan.khalid@hotmail.co.uk>
2022-12-05 23:06:18 -0500

    Project 3

---
+++ src/test/java/test3/Class3.java
@@ -0,0 +1,31 @@
+package test3;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Class3 {
+public static WebDriver driver;
+
+@Test
+public void ScreenRecording() throws Exception {
+	
+	ScreenRecorderUtil.startRecord("ScreenRecording");
+	
+	driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("http://rediff.com");
+	driver.findElement(By.cssSelector("a.signin")).click();
+	driver.findElement(By.cssSelector("input.signinbtn")).click();
+	Alert alert = driver.switchTo().alert();
+	alert.accept();
+	 driver.findElement(By.id("login1")).sendKeys("Farhankhalid920@hotmail.com");
+     driver.findElement(By.id("password")).sendKeys("subhan");
+     driver.quit();
+     ScreenRecorderUtil.stopRecord();
+	
+	
+}
+}
